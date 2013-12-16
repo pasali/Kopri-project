d9b601b2f7d3e0fcff15b7acde38ed434d3e55c2
Mehmet Başal <mhmtbsl@bil.omu.edu.tr>
2013-12-16 17:19:51 +0200

    performance

--- Kopri/src/com/pasali/kopri/MainActivity.java
+++ Kopri/src/com/pasali/kopri/MainActivity.java
@@ -1,8 +1,5 @@
 package com.pasali.kopri;
-import java.io.IOException;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -10,13 +7,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-
 public class MainActivity extends Activity implements View.OnClickListener {
-	
+
 	private EditText ip;
 	private String ip_addr;
-	public static DatagramSocket s;
-	public static InetAddress serverAddr;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,31 +22,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
 	}
 
-
 	public void onClick(View v) {
 		ip_addr = ip.getText().toString();
-		new Thread(new ClientThread()).start();
-	}
-
-
-	class ClientThread implements Runnable {
-
-		@Override
-		public void run() {
-			try {
-				s = new DatagramSocket();
-				serverAddr = InetAddress.getByName(ip_addr);
-				Intent touchIntent = new Intent(getApplicationContext(),
-						TouchActivity.class);
-				startActivity(touchIntent);
-			} catch (UnknownHostException e1) {
-				System.err.println("Bilinmeyen Sunucu");
-			} catch (IOException e1) {
-				System.err.println("Bağlantı Kurulamadı.");
-			}
-
-		}
-
+		Intent touchIntent = new Intent(getApplicationContext(),
+				TouchActivity.class);
+		touchIntent.putExtra("ip_addr", ip_addr);
+		startActivity(touchIntent);
 	}
 
 }
--- Kopri/src/com/pasali/kopri/TouchActivity.java
+++ Kopri/src/com/pasali/kopri/TouchActivity.java
@@ -1,13 +1,13 @@
 package com.pasali.kopri;
+
 import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.DatagramPacket;
-import java.net.UnknownHostException;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
-import android.view.Menu;
 import android.view.MotionEvent;
-
 import android.widget.Button;
 
 public class TouchActivity extends Activity implements View.OnClickListener {
@@ -15,6 +15,8 @@ public class TouchActivity extends Activity implements View.OnClickListener {
 	private int pre_x, pre_y;
 	private String distance_x, distance_y;
 	private String click = "";
+	private DatagramSocket s;
+	private InetAddress serverAddr;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,32 +32,30 @@ public class TouchActivity extends Activity implements View.OnClickListener {
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		final int action = event.getAction();
+
 		switch (action & MotionEvent.ACTION_MASK) {
-
-		case MotionEvent.ACTION_DOWN: {
-			pre_x = (int) event.getX();
-			pre_y = (int) event.getY();
-			break;
-		}
-		
-		case MotionEvent.ACTION_UP: {
-			long delay = event.getEventTime() - event.getDownTime();
-			if (delay < 200) {
-				click = "left";
-				new Thread(new ClientThread()).start();
+			case MotionEvent.ACTION_DOWN: {
+				pre_x = (int) event.getX();
+				pre_y = (int) event.getY();
+				break;
 			}
-			break;
-		}
-
-		case MotionEvent.ACTION_MOVE: {
-			distance_x = String.valueOf((int) event.getX() - pre_x);
-			distance_y = String.valueOf((int) event.getY() - pre_y);
-			pre_x = (int) event.getX();
-			pre_y = (int) event.getY();
-			click = "";
-			new Thread(new ClientThread()).start();
-			break;
-		}
+			case MotionEvent.ACTION_UP: {
+				long delay = event.getEventTime() - event.getDownTime();
+				if (delay < 200) {
+					click = "left";
+					new Thread(new ClientThread()).start();
+				}
+				break;
+			}
+			case MotionEvent.ACTION_MOVE: {
+				distance_x = String.valueOf((int) event.getX() - pre_x);
+				distance_y = String.valueOf((int) event.getY() - pre_y);
+				pre_x = (int) event.getX();
+				pre_y = (int) event.getY();
+				click = "";
+				new Thread(new ClientThread()).start();
+				break;
+			}
 		}
 		return true;
 	}
@@ -63,44 +63,40 @@ public class TouchActivity extends Activity implements View.OnClickListener {
 	public void onClick(View v) {
 
 		switch (v.getId()) {
-		case R.id.button1:
-			click = "left";
-			break;
-		case R.id.Button2:
-			click = "right";
-			break;
-		default:
-			break;
+			case R.id.button1:
+				click = "left";
+				break;
+			case R.id.Button2:
+				click = "right";
+				break;
+			default:
+				break;
 		}
 		new Thread(new ClientThread()).start();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
 	}
 
 	class ClientThread implements Runnable {
 
 		@Override
 		public void run() {
-			String pkg;
+			String pkg, ip_addr = null;
 			try {
 				if (click.equals("")) {
 					pkg = distance_x + "," + distance_y;
-				}else {
+				} else {
 					pkg = click;
 				}
+				Bundle extras = getIntent().getExtras();
+				if (extras != null) {
+					ip_addr = extras.getString("ip_addr");
+				}
+				s = new DatagramSocket();
+				serverAddr = InetAddress.getByName(ip_addr);
 				byte[] Data = new byte[1024];
 				Data = pkg.getBytes("UTF-8");
 				DatagramPacket sendPacket = new DatagramPacket(Data,
-						Data.length, MainActivity.serverAddr, 9876);
-
-				MainActivity.s.send(sendPacket);
-			} catch (UnknownHostException e1) {
-				System.err.println("Bilinmeyen Sunucu");
+						Data.length, serverAddr, 9876);
+				s.send(sendPacket);
 			} catch (IOException e1) {
 				System.err.println("Bağlantı Kurulamadı.");
 			}
